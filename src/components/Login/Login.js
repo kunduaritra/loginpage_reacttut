b72394dfd4ddfa032f08023f74951f60d4697756
kunduaritra <kunduaritra7@gmail.com>
2023-07-20 22:27:52 +0530

    useContext and AuthContext added for input

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,11 @@
-import React, { useState, /*{useEffect,}*/ useReducer } from 'react';
+import React, { useState, /*{useEffect},*/ useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../context/auth-context';
+import Input from '../UI/Input/Input';
+
 
 const emailReducer = (state, action) => {
   if(action.type === 'USER_INPUT'){
@@ -44,11 +47,14 @@ const Login = (props) => {
   })
   useReducer()
 
+  const authCtx = useContext(AuthContext)
+  const {isValid: emailIsValid} = emailState
+  const {isValid: passwordIsValid} = passwordState
   // useEffect(() => {
   //   const timerIndetifier = setTimeout(() => {
   //     console.log('Chekcing form validity')
   //     setFormIsValid(
-  //       enteredEmail.includes('@') && enteredPassword.trim().length > 6 && enteredCollege.trim().length > 3
+  //       enteredEmail.includes('@') && enteredPassword.trim().length > 6
   //     );
   //   },500);
 
@@ -56,7 +62,7 @@ const Login = (props) => {
   //     console.log('Clean Up!')
   //     clearTimeout(timerIndetifier)
   //   };
-  // },[enteredEmail, enteredPassword, enteredCollege])
+  // },[enteredEmail, enteredPassword])
 
   const emailChangeHandler = (event) => {
     dispatchEmail({type: 'USER_INPUT', val: event.target.value});
@@ -91,40 +97,30 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            emailState.isValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={emailState.value}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
+        <Input 
+          id="email" 
+          label="E-mail" 
+          type="email" 
+          isValid={emailIsValid} 
+          value={emailState.value}
+          onChange={emailChangeHandler}
+          onBlur= {validateEmailHandler}
           />
-        </div>
-        <div
-          className={`${classes.control} ${
-            passwordState.isValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={passwordState.value}
-            onChange={passwordChangeHandler}
-            onBlur={validatePasswordHandler}
+        <Input 
+          id="password" 
+          label="Password" 
+          type="password" 
+          isValid={passwordIsValid} 
+          value={passwordState.value}
+          onChange={passwordChangeHandler}
+          onBlur= {validatePasswordHandler}
           />
-        </div>
         {/* <div className={`${classes.control} ${
             passwordIsValid === false ? classes.invalid : ''
           }`}>
